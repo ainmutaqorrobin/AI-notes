9e75fa814b926422f2dadd63be3bce651a310929
ainmutaqorrobin <ainmutaqorrobin@gmail.com>
2025-06-30 10:04:43 +0800

    add noteEmbedding schema for vector searching

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,11 +4,24 @@ import { v } from "convex/values";
 
 const schema = defineSchema({
   ...authTables,
+
   notes: defineTable({
     title: v.string(),
     body: v.string(),
     userId: v.id("users"),
   }).index("by_userId", ["userId"]),
+
+  noteEmbeddings: defineTable({
+    content: v.string(),
+    embedding: v.array(v.float64()),
+    noteId: v.id("notes"),
+    userId: v.id("users"),
+  })
+    .index("by_noteId", ["noteId"])
+    .vectorIndex("by_embedding", {
+      vectorField: "embedding",
+      dimensions: 1536,
+    }),
 });
 
 export default schema;
